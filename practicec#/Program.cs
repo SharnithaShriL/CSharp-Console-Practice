c20adebeacb1226f43780f4115c87c41ad77388f
SharnithaShriL <sharnitha.shri@dxc.com>
2025-12-23 13:41:35 +0530

    Add project files.

---
+++ practicec#/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace practicec_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Multiply Three Numbers
+            //int num = 3;
+            //int mul = 1;
+            //List<int> val = new List<int>();
+            //for(int i=0;i<num;i++)
+            //{
+            //    Console.WriteLine($"Enter {i} no: ");
+            //    int input = int.Parse(Console.ReadLine());
+            //    val.Add(input);
+            //}
+            //for(int i=0;i<val.Count;i++)
+            //{
+            //    mul = mul * val[i];
+            //}
+            //Console.WriteLine("Multiplication of three no is : "+mul);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //// Swap two numbers...
+            //Console.WriteLine("enter frist no: ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter second no:");
+            //int num2 = int.Parse(Console.ReadLine());
+            //int temp = 0;
+            //temp = num1;
+            //num1 = num2;
+            //num2 = temp;
+            //Console.WriteLine("---OUTPUT---");
+            //Console.WriteLine($"NUM 1 IS :{num1}");
+            //Console.WriteLine($"NUM 2 IS :{num2}");
+
+
+
+
+
+            //Console.WriteLine("This is Sharnitha...");
+            //Console.WriteLine("Sum"+(1 + 2));
+            //Console.WriteLine("Divide"+(10 / 5));
+        }
+    }
+}
