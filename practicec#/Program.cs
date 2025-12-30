12fb7482c77349ab5ab70fdccf71f1fb1610a27d
SharnithaShriL <sharnitha.shri@dxc.com>
2025-12-30 12:47:30 +0530

    commit

--- practicec#/Program.cs
+++ practicec#/Program.cs
@@ -1,27 +1,121 @@
-﻿namespace practicec_
+﻿using System.Drawing;
+
+namespace practicec_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //--------------29-12-2025-------------
+
+
+
+
+            //Console.WriteLine("Enter 1st integer");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter 2nd integer");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //if(num1>0 && num2<0)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else if(num1<0 && num2>0)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+
+
+
+
+
+            //Swap First and Last Characters
+            //Console.WriteLine("enter word");
+            //string word = Console.ReadLine();
+
+            //char[] word_char = word.ToCharArray();
+            //char temp = word_char[0];
+            //word_char[0] = word_char[word_char.Length - 1];
+            //word_char[word_char.Length - 1] = temp;
+
+            //string res = new string(word_char);
+            //Console.WriteLine(res);
+
+
+
+
+            ////Remove Character by Index
+            //Console.WriteLine("Enter word:");
+            //string str_word = Console.ReadLine();
+            //Console.WriteLine("enter what word to remove:");
+            //char str_char_remove = char.Parse(Console.ReadLine());
+            //if(str_word !=null)
+            //{
+            //    for (int i = 0; i < str_word.Length; i++)
+            //    {
+            //        if(str_word[i] == str_char_remove)
+            //        {
+            //            continue;
+
+            //        }
+            //        else
+            //        {
+            //            Console.Write(str_word[i]);
+            //        }
+            //    }
+            //}
+
+
+
+
+            ////Rectangle Pattern with Number
+            //Console.WriteLine("Enter number: ");
+            //int num = int.Parse(Console.ReadLine());
+            //for(int i=0; i<num;i++)
+            //{
+            //    if(i==0 || i==num-1)
+            //    {
+            //        for(int j=0;j<=3;j++)
+            //        {
+            //            Console.Write(num);
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        Console.Write($"{num} {num}");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+
+
             //--------------24-12-2025-------------
             //Repeat Number in Rows
-            Console.WriteLine("Enter number: ");
-            int num = int.Parse(Console.ReadLine());
-            for(int i=0;i<2;i++)
-            {
-                for(int j=0;j<4;j++)
-                {
-                    Console.Write(num+" ");
-                }
-                Console.WriteLine();
-                for (int j = 0; j < 4; j++)
-                {
-                    Console.Write(num);
-                }
-                Console.WriteLine();
+            //Console.WriteLine("Enter number: ");
+            //int num = int.Parse(Console.ReadLine());
+            //for(int i=0;i<2;i++)
+            //{
+            //    for(int j=0;j<4;j++)
+            //    {
+            //        Console.Write(num+" ");
+            //    }
+            //    Console.WriteLine();
+            //    for (int j = 0; j < 4; j++)
+            //    {
+            //        Console.Write(num);
+            //    }
+            //    Console.WriteLine();
 
-            }
+            //}
 
 
             ////Print Age Message
